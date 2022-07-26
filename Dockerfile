08449d979c617d67479b516b85f896d74acf9c08
Erikson Felipe Napan Villagaray <enapanvi@emeal.nttdata.com>
2022-07-25 21:17:30 -0500

    delete

--- Dockerfile
+++
@@ -1,4 +0,0 @@
-FROM adoptopenjdk/openjdk11:alpine-jre
-COPY target/ms_Transaction-0.0.1-SNAPSHOT.jar app.jar
-EXPOSE 8004
-ENTRYPOINT ["java","-jar","/app.jar"]
--- src/main/java/com/api/rest/springboot/webflux/transaction/service/TransactionService.java
+++
@@ -1,8 +0,0 @@
-package com.api.rest.springboot.webflux.transaction.service;
-
-import com.api.rest.springboot.webflux.transaction.model.Transaction;
-import com.api.rest.springboot.webflux.transaction.util.Crud;
-
-public interface TransactionService extends Crud<Transaction, String>{
-
-}
